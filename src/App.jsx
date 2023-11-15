0bdb1d695145245e2a0485478e4df6fedc03cd02
jolina <jolina4526@gmail.com>
2023-11-15 19:21:55 +0800

    Add firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Image } from "@nextui-org/react";
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div></div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,8 +19,25 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="bg-stone-800">
+        <h1>haaaa</h1>
+        <Button color="primary">Button</Button>
+        <Image
+          isBlurred
+          width={240}
+          src="https://nextui-docs-v2.vercel.app/images/album-cover.png"
+          alt="NextUI Album Cover"
+          classNames="m-5"
+        />
+        <Image
+          isZoomed
+          width={240}
+          alt="NextUI Fruit Image with Zoom"
+          src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
+        />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
